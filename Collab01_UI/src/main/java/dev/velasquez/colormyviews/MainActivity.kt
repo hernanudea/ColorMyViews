879c8a001e973383bed562d598af0f70276cd324
Hernan Velasquez <hernanudea@gmail.com>
2023-08-31 16:39:45 -0500

    7. Task: Create a chain of TextView views

--- Collab01_UI/src/main/java/dev/velasquez/colormyviews/MainActivity.kt
+++ Collab01_UI/src/main/java/dev/velasquez/colormyviews/MainActivity.kt
@@ -2,25 +2,12 @@ package dev.velasquez.colormyviews
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import dev.velasquez.colormyviews.ui.theme.ColorMyViewsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val color = R.color.purple_200
-        val colo2 = R.color.purple_700
-
-        val name = R.string.app_name
 
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ColorMyViews
+
+[Android Kotlin Fundamentals: 02.3 ConstraintLayout using the Layout Editor](https://developer.android.com/codelabs/kotlin-android-training-constraint-layout#0)
